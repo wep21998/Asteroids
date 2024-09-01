b795519a8985fb7cb3b7d3e55537dbab3279ec24
wep21998 <we.perry.8@hotmail.com>
2024-08-31 23:58:58 -0400

    Added astroid spawning

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    # Game inizialization
+    # Game initialization
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    #conainer initialization
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     # Game start info
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     # Set game window
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    # Initialize main game loop
+    asteroidfield = AsteroidField()
+    # initialize main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
