278b599975df25797e2fee6330d859b147bdcb6a
wep21998 <we.perry.8@hotmail.com>
2024-08-31 20:08:12 -0400

    Added main.py and constants.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from contstants import *
+def main():
+
+    # Game start info
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
