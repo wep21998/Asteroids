7b94b58d7080e857fb9bbf340b6bc7b20136beb9
wep21998 <we.perry.8@hotmail.com>
2024-08-31 22:34:23 -0400

    The player can now move!

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+
 def main():
     # Game inizialization
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     # Game start info
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     # Set game window
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # Initialize main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        player.draw(screen)
         dt = clock.tick(60) /1000
+        screen.fill((0,0,0))
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
 
