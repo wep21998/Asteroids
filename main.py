84f2b035d882be8895f6b2ef0b4f8c14a51b6603
wep21998 <we.perry.8@hotmail.com>
2024-08-31 20:42:28 -0400

    You can now quit the game

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     # Initialize main game loop
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0,0,0))
         pygame.display.flip()
 
