45cf8120935eeb68a328c5a5a61f064f674c6063
wep21998 <we.perry.8@hotmail.com>
2024-08-31 20:31:13 -0400

    Added functionality for screen

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
-from contstants import *
+from constants import *
 def main():
-
+    # Game inizialization
+    pygame.init()
     # Game start info
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
-
-
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    # Set game window
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    # Initialize main game loop
 
 
 
